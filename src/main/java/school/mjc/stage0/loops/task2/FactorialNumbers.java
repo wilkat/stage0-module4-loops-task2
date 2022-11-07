78725b3e3b2919bdfb2fd13e6a898f871a314706
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-07 22:20:56 +0100

    new

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-        int i = 0;
-        int fact= 0;
+        int i = 1;
+        int fact= 1;
         while (i<=printToInclusive) {
             fact = fact*i;
             System.out.println(fact);
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -4,7 +4,7 @@ public class WordsBuilder {
     public void buildPhrase(char... chars) {
         int i = 0;
         String sent = "";
-        while(i <= chars.length) {
+        while(i < chars.length) {
             sent = sent+chars[i];
             i++;
         }
