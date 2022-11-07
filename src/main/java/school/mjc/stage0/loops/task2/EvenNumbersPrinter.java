6ae2240b499af9d17a0a1c03e528fa0bd6ca2739
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-07 22:08:21 +0100

    new

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
-        int i = 0;
+        int i = 1;
         while (i <= printTillInclusive) {
             if(i%2 == 0) {
                 System.out.println(i);
